9a7c366a1f34c8ccb2728f33d7bcca770c672df7
e-kuri <eugenio.kuri@gmail.com>
2019-04-29 15:15:26 -0500

    removed unused commented code

--- app/src/main/java/com/kuri/happygrowing/stats/viewmodel/StatsViewModelFactory.kt
+++ app/src/main/java/com/kuri/happygrowing/stats/viewmodel/StatsViewModelFactory.kt
@@ -4,15 +4,7 @@ import androidx.lifecycle.LifecycleOwner
 import com.kuri.happygrowing.shared.callback.OnResultCallback
 import com.kuri.happygrowing.shared.logging.getLogger
 import com.kuri.happygrowing.stats.model.Measurement
-import com.kuri.happygrowing.stats.model.SensorType
 import com.kuri.happygrowing.stats.repository.measurement.StatsRepositoryFactory
-
-/*
-object StatsViewModelFactory: ViewModelProvider.Factory {
-
-    override fun <T : ViewModel?> create(modelClass: Class<T>) = CurrentStatsViewModel(StatsRepositoryFactory.repo, getLogger()) as T
-}
-*/
 
 object StatsViewModelFactory {
 
