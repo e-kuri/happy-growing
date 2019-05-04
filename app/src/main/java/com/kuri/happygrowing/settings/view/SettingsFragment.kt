cce32ddc3f7efa1dc0a143351f186d6e68c23f5b
e-kuri <eugenio.kuri@gmail.com>
2019-05-04 14:58:42 -0500

    preferences view on place

--- app/src/main/java/com/kuri/happygrowing/settings/view/SettingsFragment.kt
+++ app/src/main/java/com/kuri/happygrowing/settings/view/SettingsFragment.kt
@@ -1,15 +1,21 @@
 package com.kuri.happygrowing.settings.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceFragmentCompat
 import com.kuri.happygrowing.R
 
-class SettingsFragment: Fragment() {
+class SettingsFragment: PreferenceFragmentCompat() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-        inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+        val minTempPref = findPreference<EditTextPreference>("min_temp")
+        minTempPref?.setOnPreferenceChangeListener{ _, newValue ->
+            Toast.makeText(context, newValue.toString(), Toast.LENGTH_SHORT).show()
+            true
+        }
+        minTempPref?.summary = "papas"
+    }
     
 }
